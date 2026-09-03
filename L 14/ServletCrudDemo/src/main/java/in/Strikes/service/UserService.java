7a9a550f985bbe8b8df9a9ffc673e9d89aed3e60
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-09-03 22:54:58 +0530

    Servlet crud

---
+++ L 14/ServletCrudDemo/src/main/java/in/Strikes/service/UserService.java
@@ -0,0 +1,26 @@
+package in.Strikes.service;
+
+import in.Strikes.model.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserService {
+    private Map<Integer , User> userDB ;
+    public UserService(){
+        userDB = new HashMap<>();
+    }
+public User createUser(User userReq){
+       userDB.put(userReq.getId(), userReq);
+       return userReq;
+}
+
+public List<User> getAllUsers(){
+ return (List<User>) userDB.values();
+}
+public User getUserById(Integer id){
+        return userDB.getOrDefault(id,null);
+}
+
+}
