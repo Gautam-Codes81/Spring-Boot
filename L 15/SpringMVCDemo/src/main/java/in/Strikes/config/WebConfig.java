18d9de19e090a4a9f05380cfeac234a1ac2345b6
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-09-11 23:43:53 +0530

    update

---
+++ L 15/SpringMVCDemo/src/main/java/in/Strikes/config/WebConfig.java
@@ -0,0 +1,13 @@
+package in.Strikes.config;
+
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "in.strikes")
+@EnableWebMvc
+public class WebConfig {
+
+}
