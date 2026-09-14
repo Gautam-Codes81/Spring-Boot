478a4eb4053c47a3905eab040a8b849a22b5e00a
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-09-14 16:22:07 +0530

    update

--- l 16/DtoDemo/crudSpringBootDemo/src/main/java/in/strikes/crudSpringBootDemo/CrudSpringBootDemoApplication.java
+++ l 16/DtoDemo/crudSpringBootDemo/src/main/java/in/strikes/crudSpringBootDemo/CrudSpringBootDemoApplication.java
@@ -10,10 +10,10 @@ public class CrudSpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringBootDemoApplication.class, args);
 //		System.out.println("gk");
-		System.out.println("gk");
-		System.out.println("GK");
-		System.out.println("rk");
-		System.out.println("rk");
+//		System.out.println("gk");
+//		System.out.println("GK");
+//		System.out.println("rk");
+//		System.out.println("rk");
 
 
 
